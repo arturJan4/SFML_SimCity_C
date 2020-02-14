44d5937d4a404be696eaff810542a2248d849d9e
arturJan4 <jankes433@gmail.com>
2020-02-14 12:40:54 +0100

    modified

--- GameLoop.cpp
+++ GameLoop.cpp
@@ -49,7 +49,7 @@ void GameLoop::loadTextures()
     //TILEMAP
     m_data->m_TileMap["grass"] = Tile(m_data->graphics.getTexture("grass"),TileType::EMPTY,tileSize,0,6.0f,0);
     //m_data->m_TileMap[""] = Tile(m_data->graphics.getTexture("forest"),TileType::FOREST,tileSize,0,6.0f,0);
-    m_data->m_TileMap["road"] = Tile(m_data->graphics.getTexture("road"),TileType::ROAD,tileSize,0,6.0f,0);
+    m_data->m_TileMap["road"] = Tile(m_data->graphics.getTexture("road"),TileType::ROAD,tileSize,0,3.0f,0);
     m_data->m_TileMap["forest"] = Tile(m_data->graphics.getTexture("forest"),TileType::FOREST,tileSize,0,6.0f,0);
     m_data->m_TileMap["industrial"] = Tile(m_data->graphics.getTexture("industrial"),TileType::INDUSTRIAL,tileSize,3,1.0f,0);
     m_data->m_TileMap["residential"] = Tile(m_data->graphics.getTexture("residential"),TileType::RESIDENTIAL,tileSize,0,6.0f,0);
